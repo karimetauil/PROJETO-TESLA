0916e597dd447acfd82c36199b7716173ecc80a2
karimetauil <karimeupd@hotmail.com>
2024-07-16 17:26:34 -0300

    tesla

---
+++ scipts.js
@@ -0,0 +1,10 @@
+let formulario = document.querySelector(".formulario")
+
+
+function aparecerFormulario() {
+    formulario.style.left = "50%"
+}
+
+function desaparecerFormulario() {
+    formulario.style.left = "-50%"
+}
